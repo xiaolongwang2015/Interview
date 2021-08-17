ddf707ff2e114ca030f181c878233f97b550984b
wangxiaolong <wangxiaolong@in-deco.com>
2021-08-17 17:54:28 +0800

    200 dfs

--- 200岛屿数量.py
+++ 200岛屿数量.py
@@ -34,7 +34,6 @@ def numIslands(grid: list) -> int:
     :param grid: 0表示水 1表示陆地
     :return: 岛屿数量
     测试样例：
-
     >>> numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]])
     1
     >>> numIslands([["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]])
@@ -43,45 +42,46 @@ def numIslands(grid: list) -> int:
     0
     """
     rst = 0
-    for column_idx in range(len(grid)):
-        for row_idx in range(len(grid[0])):
-            if grid[column_idx][row_idx] == '1':
+    row = len(grid)
+    column = len(grid[0])
+    for row_idx in range(row):
+        for column_idx in range(column):
+            if grid[row_idx][column_idx] == '1':
                 rst += 1
+                island_dfs(grid, row_idx, column_idx, row, column)
             else:
                 continue
     return rst
 
 
-def island_dfs(grid, column_idx, row_idx):
-    grid[column_idx][row_idx] = '0'
+def island_dfs(grid, row_idx, column_idx, row, column):
+    grid[row_idx][column_idx] = '0'
+    for x, y in surround_idx(row_idx, column_idx):
+        if 0 <= x < row and 0 <= y < column and grid[x][y] == '1':
+            island_dfs(grid, x, y, row, column)
+
+    # if 0 <= row_idx + 1 < row and grid[row_idx + 1][column_idx] == '1':
+    #     island_dfs(grid, row_idx + 1, column_idx, row, column)
+    # if 0 <= row_idx - 1 < row and grid[row_idx - 1][column_idx] == '1':
+    #     island_dfs(grid, row_idx - 1, column_idx, row, column)
+    # if 0 <= column_idx + 1 < column and grid[row_idx][column_idx + 1] == '1':
+    #     island_dfs(grid, row_idx, column_idx + 1, row, column)
+    # if 0 <= column_idx - 1 < column and grid[row_idx][column_idx - 1] == '1':
+    #     island_dfs(grid, row_idx, column_idx - 1, row, column)
 
 
+def island_bfs(grid, row_idx, column_idx, row, column):
+    pass
 
-# class Solution:
-#     def dfs(self, grid, r, c):
-#         grid[r][c] = 0
-#         nr, nc = len(grid), len(grid[0])
-#         for x, y in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
-#             if 0 <= x < nr and 0 <= y < nc and grid[x][y] == "1":
-#                 self.dfs(grid, x, y)
-#
-#     def numIslands(self, grid: List[List[str]]) -> int:
-#         nr = len(grid)
-#         if nr == 0:
-#             return 0
-#         nc = len(grid[0])
-#
-#         num_islands = 0
-#         for r in range(nr):
-#             for c in range(nc):
-#                 if grid[r][c] == "1":
-#                     num_islands += 1
-#                     self.dfs(grid, r, c)
-#
-#         return num_islands
+
+def surround_idx(row_idx, column_idx):
+    yield row_idx + 1, column_idx
+    yield row_idx - 1, column_idx
+    yield row_idx, column_idx + 1
+    yield row_idx, column_idx - 1
 
 
 if __name__ == '__main__':
     import doctest
 
-    # doctest.testmod()
+    doctest.testmod()
