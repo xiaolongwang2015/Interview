a46c3627d703e3a603d9e7ec5d00819ae83a2c8a
wangxiaolong <wangxiaolong@in-deco.com>
2021-08-23 18:48:01 +0800

    133 207

---
+++ bfs&dfs/207课程表.py
@@ -0,0 +1,66 @@
+"""
+207 课程表
+难度：中等
+题目描述：
+        你这个学期必须选修 numCourses 门课程，记为0到numCourses - 1 。
+        在选修某些课程之前需要一些先修课程。 先修课程按数组prerequisites 给出，
+        其中prerequisites[i] = [ai, bi] ，表示如果要学习课程ai 则 必须 先学习课程 bi 。
+        例如，先修课程对[0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+        请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+
+示例1：
+        输入：numCourses = 2, prerequisites = [[1,0]]
+        输出：true
+        解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。
+
+示例2：
+        输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+        输出：false
+        解释：总共有 2 门课程。学习课程 1 之前，你需要先完成课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+
+
+
+题解方法：
+
+"""
+
+
+def canFinish(numCourses: int, prerequisites: list) -> bool:
+    """
+    判断是否可能完成所有课程的学习
+    Note: 1 1 <= numCourses <= 10^5
+          2 prerequisites[i].length == 2
+          3 0 <= ai, bi < numCourses
+          4 prerequisites[i] 中的所有课程对 互不相同
+    :param numCourses:
+    :param prerequisites:
+    :return:
+    测试样例：
+    >>> canFinish()
+    """
+
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
